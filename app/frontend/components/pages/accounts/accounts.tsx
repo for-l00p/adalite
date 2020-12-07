bc993e6f4c65da31879eae7a4722b0c82267e381
David Tran Duc <david.tran.duc@vacuumlabs.com>
2020-12-07 17:46:38 +0100

    Add tablet/mobile friendly views and info sidebar

--- app/frontend/components/pages/accounts/accounts.tsx
+++ app/frontend/components/pages/accounts/accounts.tsx
@@ -5,6 +5,7 @@ import range from '../../../../frontend/wallet/helpers/range'
 import printAda from '../../../helpers/printAda'
 import {Lovelace} from '../../../state'
 import {AdaIcon} from '../../common/svg'
+import Alert from '../../common/alert'
 
 const Account = ({i, account, setAccount, selectedAccount}) => {
   const buttonLabel = () => {
@@ -12,11 +13,22 @@ const Account = ({i, account, setAccount, selectedAccount}) => {
     if (!account) return `Explore #${i}`
     return `Activate #${i}`
   }
+  const Balance = ({value}: {value: Lovelace}) => (
+    <Fragment>
+      {printAda(value, 3)}
+      <AdaIcon />
+    </Fragment>
+  )
+
   return (
-    <div key={i} className="card account" style={'width: 75%;'}>
-      <div className="card-column" style="align-self: center; width">
+    <div key={i} className="card account">
+      <div className="header-wrapper mobile">
+        <h2 className="card-title small-margin">Account {i}</h2>
+      </div>
+      <div className="card-column account-button-wrapper">
+        <h2 className="card-title small-margin account-header desktop">Account {i}</h2>
         <button
-          className="button primary"
+          className="button primary nowrap"
           disabled={selectedAccount === i}
           onClick={() => {
             setAccount(i)
@@ -25,28 +37,29 @@ const Account = ({i, account, setAccount, selectedAccount}) => {
           {buttonLabel()}
         </button>
       </div>
-      <div className="card-column">
+      <div className="card-column account-item-info-wrapper">
         <h2 className="card-title small-margin">Available balance</h2>
-        <div className="balance-amount small">
-          {printAda(
-            account
-              ? account.shelleyBalances.stakingBalance + account.shelleyBalances.nonStakingBalance
-              : 0,
-            3
+        <div className="balance-amount small item">
+          {account ? (
+            <Balance
+              value={
+                account.shelleyBalances.stakingBalance + account.shelleyBalances.nonStakingBalance
+              }
+            />
+          ) : (
+            '-'
           )}
-          <AdaIcon />
         </div>
       </div>
-      <div className="card-column">
+      <div className="card-column account-item-info-wrapper">
         <h2 className="card-title small-margin">Rewards balance</h2>
-        <div className="balance-amount small">
-          {printAda(account ? account.shelleyBalances.rewardsAccountBalance : 0, 3)}
-          <AdaIcon />
+        <div className="balance-amount small item">
+          {account ? <Balance value={account.shelleyBalances.rewardsAccountBalance} /> : '-'}
         </div>
       </div>
-      <div className="card-column">
+      <div className="card-column account-item-info-wrapper">
         <h2 className="card-title small-margin">Delegation</h2>
-        <div className="delegation-account">
+        <div className="delegation-account item">
           {account ? account.shelleyAccountInfo.delegation.ticker || '-' : '-'}
         </div>
       </div>
@@ -65,47 +78,85 @@ const Accounts = ({accounts, setAccount, selectedAccount, reloadWalletInfo}) => 
     (a, {shelleyBalances}) => shelleyBalances.rewardsAccountBalance + a,
     0
   )
+  const InfoAlert = () => (
+    <Fragment>
+      <div className="dashboard-column account sidebar-item info">
+        <Alert alertType="info sidebar">
+          <p>
+            <strong>Accounts</strong> offer a way to split your funds. You are able to delegate to
+            different pool from each account. Each account has different set of addresses and keys.
+          </p>
+        </Alert>
+      </div>
+      <div className="dashboard-column account sidebar-item info">
+        <Alert alertType="info sidebar">
+          <p>
+            Click explore/activate button to load data for related account. If you are using
+            hardware wallet, you will be requested to export public key.
+          </p>
+        </Alert>
+      </div>
+      <div className="dashboard-column account info">
+        <Alert alertType="warning sidebar">
+          <p>
+            This feature might not be supported on other wallets yet. If you decide to move your
+            funds to <strong>account</strong> other then <strong>account 0</strong>, you might not
+            see your funds in other wallets.
+          </p>
+        </Alert>
+      </div>
+    </Fragment>
+  )
 
   return (
     <Fragment>
       <div className="dashboard-column account">
-        <div className="card account" style={'width: 100%;'}>
-          <div>
-            <h2 className="card-title small-margin">Total balance</h2>
-            <div className="balance-amount">
-              {printAda(totalBalance as Lovelace)}
-              <AdaIcon />
+        <div className="card account-aggregated">
+          <div className="balance">
+            <div className="item">
+              <h2 className="card-title small-margin">Total balance</h2>
+              <div className="balance-amount">
+                {printAda(totalBalance as Lovelace)}
+                <AdaIcon />
+              </div>
+            </div>
+            <div className="item">
+              <h2 className="card-title small-margin">Total rewards balance</h2>
+              <div className="balance-amount">
+                {printAda(totalRewardBalance as Lovelace)}
+                <AdaIcon />
+              </div>
             </div>
           </div>
-          <div>
-            <h2 className="card-title small-margin">Total rewards balance</h2>
-            <div className="balance-amount">
-              {printAda(totalRewardBalance as Lovelace)}
-              <AdaIcon />
-            </div>
-          </div>
-          <div className="card-column">
+          <div className="refresh-wrapper">
             <button className="button secondary balance refresh" onClick={reloadWalletInfo}>
               Refresh
             </button>
           </div>
         </div>
-        <div className="dashboard-column account wide">
-          <div>
-            {range(0, Object.keys(accounts).length + 1).map(
-              (i) =>
-                (i === 0 || accounts[i - 1].isUsed) && (
-                  <Account
-                    key={i}
-                    i={i}
-                    account={accounts[i]}
-                    setAccount={setAccount}
-                    selectedAccount={selectedAccount}
-                  />
-                )
-            )}
+        <div className="mobile">
+          <InfoAlert />
+        </div>
+        <div className="accounts-wrapper">
+          <div className="dashboard-column account list">
+            <div>
+              {range(0, Object.keys(accounts).length + 1).map(
+                (i) =>
+                  (i === 0 || accounts[i - 1].isUsed) && (
+                    <Account
+                      key={i}
+                      i={i}
+                      account={accounts[i]}
+                      setAccount={setAccount}
+                      selectedAccount={selectedAccount}
+                    />
+                  )
+              )}
+            </div>
           </div>
-          <div className="dashboard-column account narrow"> </div>
+          <div className="desktop">
+            <InfoAlert />
+          </div>
         </div>
       </div>
     </Fragment>
